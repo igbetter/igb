85201e81c8cee233696b25d29a7108439529354f
tracy apps <t@thisroad.org>
2023-10-05 12:45:55 -0500

    bug fixes of browse content and glossary 10/5

--- wp-content/themes/it-gets-better-main/theme/template-parts/content/content-glossaryArchive.php
+++ wp-content/themes/it-gets-better-main/theme/template-parts/content/content-glossaryArchive.php
@@ -10,8 +10,6 @@
 
 ?>
 <div id="template-archive-glossary" <?php post_class(); ?>>
-    <?php the_content(); ?>
-    <div class="flex flex-col md:flex-row justify-center w-full h-auto">
-        <?php get_template_part( 'template-parts/content/content', 'glossaryListItem' ); ?>
-    </div>
+  <?php the_content(); ?>
+  <?php get_template_part( 'template-parts/content/content', 'glossaryListItem' ); ?>
 </div><!-- #post-${ID} -->
