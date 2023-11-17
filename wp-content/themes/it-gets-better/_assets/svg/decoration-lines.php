efd9fd6e395460a884ec8704440b4b416e3f11c7
tracy apps <t@thisroad.org>
2023-11-16 21:33:35 -0600

    updated svg frame lines

--- wp-content/themes/it-gets-better/_assets/svg/decoration-lines.php
+++ wp-content/themes/it-gets-better/_assets/svg/decoration-lines.php
@@ -29,118 +29,123 @@
 	</symbol>
 
 	<symbol  id="igb_frame_option_02" class="feature_frame" x="0px" y="0px"
-		 viewBox="0 0 100 115">
+	preserveAspectRatio="none meet"
+		 viewBox="0 -20 100 135">
 		<path class="svg_line pink"
-			  d="m 32 0
-				 l -8.5 16
-				 l -19 13
-				 l 12 18
-				 l -2 50
-				 L 110 115"/>
-		<path class="svg_line yellow"
-			  d="m 35.75 0
-				 l -12.53 25
-				 l 22 20
-				 l -24 34
-				 l 30 28
-				 l 70 -2"/>
-		<path class="svg_line blue"
-			  d="m 39.5 0
-				 l -8.8 18
-				 L 87 51
-				 l 5 -30
-				 l 10 -2"/>
+				  d="m 41.5 -20
+					 l -19 35
+					 l -19 13
+					 l 12 18
+					 l -2 50
+					 L 110 115"/>
+			<path class="svg_line yellow"
+				  d="m 45.75 -20
+					 l -22.53 43
+					 l 22 20
+					 l -24 34
+					 l 30 28
+					 l 70 -2"/>
+			<path class="svg_line blue"
+				  d="m 49.5 -20
+					 l -18 36
+					 L 87 51
+					 l 5 -30
+					 l 10 -2"/>
 	</symbol>
 
 	<symbol id="igb_frame_option_03" class="feature_frame" x="0px" y="0px"
-		 viewBox="0 0 100 115">
+		preserveAspectRatio="none meet"
+		viewBox="0 -20 100 135">
 		<path class="svg_line pink"
-			  d="m 35 0
-				 l -8.5 16
-				 L 5 42
-				 l 12 50
-				 l 0 14
-				 L 110 114"/>
-		<path class="svg_line yellow"
-			  d="m 38.75 0
-				 l -12.53 25
-				 L 2 64
-				 l 18 11
-				 l 10 28
-				 l 70 -2"/>
-		<path class="svg_line blue"
-			  d="m 42.5 0
-				 l -8.8 18
-				 l -14 47
-				 l 65 5
-				 l 20 -28"/>
+				  d="m 45 -20
+					 l -17.5 32
+					 L 5 42
+					 l 12 50
+					 l 0 14
+					 L 110 114"/>
+			<path class="svg_line yellow"
+				  d="m 48.75 -20
+					 l -22 42
+					 L 2 64
+					 l 18 11
+					 l 10 28
+					 l 70 -2"/>
+			<path class="svg_line blue"
+				  d="m 52.5 -20
+					 l -18.8 37
+					 l -14 47
+					 l 65 5
+					 l 20 -28"/>
 	</symbol>
 
 	<symbol id="igb_frame_option_04" class="feature_frame" x="0px" y="0px"
-		 viewBox="0 0 100 115">
+		preserveAspectRatio="none meet"
+		viewBox="0 -20 100 135">
 		<path class="svg_line pink"
-			  d="m 55 0
-				l -10.5 21
-				l 49 28
-				 L -2 119"/>
-		<path class="svg_line yellow"
-			  d="m 58.75 0
-				 l -15.2 30
-				 l 45 3
-				 l -9 38
-				 l 20 32"/>
-		<path class="svg_line blue"
-			  d="m 62.5 0
-				 l -9.8 20
-				 l 50 8"/>
+				  d="m 65 -20
+					l -20.5 38
+					l 49 28
+					 L -2 119"/>
+			<path class="svg_line yellow"
+				  d="m 68.75 -20
+					 l -25.8 49
+					 l 45 3
+					 l -9 38
+					 l 21.5 32"/>
+			<path class="svg_line blue"
+				  d="m 72.5 -20
+					 l -17.8 35
+					 l 50 8"/>
 	</symbol>
 
 	<symbol id="igb_frame_option_05" class="feature_frame" x="0px" y="0px"
-		 viewBox="0 0 100 115">
+		preserveAspectRatio="none meet"
+		viewBox="0 -20 100 135">
 		<path class="svg_line pink"
-			  d="m 30 0
-				 l -10 19
-				 l 12 35
-				 l -26 38
-				 l 87 9
-				 l 0 9
-				 L 102 114"/>
-		<path class="svg_line yellow"
-			  d="m 33.75 0
-				 l -15.2 30
-				 l -4 35
-				 l 9 25
-				 l 55 2
-				 l 20 -7"/>
-		<path class="svg_line blue"
-			  d="m 37.5 0
-				 l -5.8 12
-				 l 51 31
-				 l 7 -16
-				 L 103 55"/>
+				  d="m 40 -20
+					 l -17.75 33
+					 l 12 35
+					 l -26 38
+					 l 87 9
+					 l 0 9
+					 L 102 114"/>
+			<path class="svg_line yellow"
+				  d="m 43.75 -20
+					 l -22.7 44
+					 l -4 35
+					 l 9 25
+					 l 55 2
+					 l 20 -7"/>
+			<path class="svg_line blue"
+				  d="m 47.5 -20
+					 l -12.8 26
+					 l 51 31
+					 l 7 -16
+					 L 103 55"/>
 	</symbol>
 
 	<symbol id="igb_frame_option_06" class="feature_frame" x="0px" y="0px"
-		 viewBox="0 0 100 115">
+		preserveAspectRatio="none meet"
+		viewBox="0 -20 100 135">
 		<path class="svg_line pink"
-			  d="m 28 0
-				 l -7.7 15
-				 l -15 14
-				 l 21 11
-				 L 15 115"/>
-		<path class="svg_line yellow"
-			  d="m 32 0
-				 l -10.3 20
-				 l 20 25
-				 l -24 30
-				 l 32 20
-				 L 34 117"/>
-		<path class="svg_line blue"
-			  d="m 35.5 0
-				 l -8.8 18
-				 L 84 47
-				 l 8 -32
-				 L 101 22"/>
+				  d="m 38 -20
+					 l -17.7 33
+					 l -15 14
+					 l 21 11
+					 L 15 115"/>
+			<path class="svg_line yellow"
+				  d="m 42 -20
+					 l -20.3 39
+					 l 20 25
+					 l -24 30
+					 l 32 20
+					 L 34 117"/>
+			<path class="svg_line blue"
+				  d="m 45.5 -20
+					 l -15.98 32
+					 L 84 47
+					 l 8 -32
+					 L 101 22"/>
 	</symbol>
 
 	<symbol id="igb_smaller_frame_option_01" class="small_frame" x="0" y="0"
