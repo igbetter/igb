b021940b6119ed4bbe8efa4dd8d7c872ad8139c4
tracy apps <t@thisroad.org>
2023-11-18 03:08:11 -0600

    custom block category

--- wp-content/themes/it-gets-better/_assets/functions/acf.php
+++ wp-content/themes/it-gets-better/_assets/functions/acf.php
@@ -1,60 +1,124 @@
 <?php
 
+add_filter( 'block_categories_all' , function( $categories ) {
+
+	// Adding a new category.
+	$categories[] = array(
+		'slug'  => 'igb',
+		'title' => 'It Gets Better custom blocks'
+	);
+
+	return $categories;
+} );
+
 add_action('acf/init', 'igb_initialize_acf_blocks');
 
 function igb_initialize_acf_blocks() {
   // Check function exists.
-  
+
   if( function_exists('acf_register_block_type') ) {
-    // Register a Guid block block.
-    acf_register_block_type(array(
-      'name'              => 'guides-block',
-      'title'             => __('Guide - Block'),
-      'description'       => __('Home page Guide block'),
-      'render_template'   => 'template-parts/blocks/guide.php',
-      'category'          => 'igb acf blocks',
-      'example'           => array(
-        'attributes' => array(
-          'mode' => 'preview',
-          'data' => array(
-            'is_preview'    => true
-          )
-        )
-      )
-    ));
+	// Register a Guid block block.
+	acf_register_block_type(array(
+		'name' 				=> 'hero-block',
+		'title' 			=> __('Page Hero'),
+		'description'		=> __('Hero section for the top of a page'),
+		'render_template'  	=> get_template_directory() . '/acf-blocks/hero-block/hero.php',
+		'icon'				=> file_get_contents( get_template_directory() . '/acf-blocks/_block-assets/icon-svg-frame.svg' ),
+		'category' 			=> 'igb',
+		'example'  			=> array(
+			'attributes' 	=> array(
+				'mode' 		=> 'preview',
+				'data' 		=> array(
+					'is_preview'    => true
+				)
+			)
+		)
+	));
 
-    // Register a Color block.
-    acf_register_block_type(array(
-      'name'              => 'color-block',
-      'title'             => __('Color - Block'),
-      'description'       => __('Color block'),
-      'render_template'   => 'template-parts/blocks/color.php',
-      'category'          => 'igb acf blocks',
-      'example'           => array(
-        'attributes' => array(
-          'mode' => 'preview',
-          'data' => array(
-            'is_preview'    => true
-          )
-        )
-      )
-    ));
+	acf_register_block_type(array(
+		'name' 				=> 'featured-content-block',
+		'title' 			=> __('Featured Content'),
+		'description'		=> __('Feature a video, playlist, or blog post'),
+		'render_template'  	=> get_template_directory() . '/acf-blocks/featured-content-block/featured-content.php',
+		'icon'				=> file_get_contents( get_template_directory() . '/acf-blocks/_block-assets/icon-featured-content.svg' ),
+		'category' 			=> 'igb',
+		'example'  			=> array(
+			'attributes' 	=> array(
+				'mode' 		=> 'preview',
+				'data' 		=> array(
+					'is_preview'    => true
+				)
+			)
+		)
+	));
 
-    // Register a Color block.
-    acf_register_block_type(array(
-      'name'              => 'featured-block',
-      'title'             => __('Featured - Block'),
-      'description'       => __('Featured block'),
-      'render_template'   => 'template-parts/blocks/featured-blog.php',
-      'category'          => 'igb acf blocks',
-      'example'           => array(
-        'attributes' => array(
-          'mode' => 'preview',
-          'data' => array(
-            'is_preview'    => true
-          )
-        )
-      )
-    ));
+	acf_register_block_type(array(
+		'name' 				=> 'term-list-block',
+		'title' 			=> __('Glossary Term List'),
+		'description'		=> __('Popular glossary terms list'),
+		'render_template'  	=> get_template_directory() . '/acf-blocks/term-list-block/term-list.php',
+		'icon'				=> file_get_contents( get_template_directory() . '/acf-blocks/_block-assets/icon-popular-terms.svg' ),
+		'category' 			=> 'igb',
+		'example'  			=> array(
+			'attributes' 	=> array(
+				'mode' 		=> 'preview',
+				'data' 		=> array(
+					'is_preview'    => true
+				)
+			)
+		)
+	));
+
+	acf_register_block_type(array(
+		'name' 				=> 'featured-term-block',
+		'title' 			=> __('Featured Glossary Term'),
+		'description'		=> __('Feature one glossary term'),
+		'render_template'  	=> get_template_directory() . '/acf-blocks/featured-term-block/featured-term.php',
+		'icon'				=> file_get_contents( get_template_directory() . '/acf-blocks/_block-assets/icon-term.svg' ),
+		'category' 			=> 'igb',
+		'example'  			=> array(
+			'attributes' 	=> array(
+				'mode' 		=> 'preview',
+				'data' 		=> array(
+					'is_preview'    => true
+				)
+			)
+		)
+	));
+
+	acf_register_block_type(array(
+		'name' 				=> 'more-content-block',
+		'title' 			=> __('More Content'),
+		'description'		=> __('Display more content'),
+		'render_template'  	=> get_template_directory() . '/acf-blocks/more-content-block/more-content.php',
+		'icon'				=> file_get_contents( get_template_directory() . '/acf-blocks/_block-assets/icon-more-content.svg' ),
+		'category' 			=> 'igb',
+		'example'  			=> array(
+			'attributes' 	=> array(
+				'mode' 		=> 'preview',
+				'data' 		=> array(
+					'is_preview'    => true
+				)
+			)
+		)
+	));
+
+	acf_register_block_type(array(
+		'name' 				=> 'related-content-block',
+		'title' 			=> __('Related Content'),
+		'description'		=> __('Display related content'),
+		'render_template'  	=> get_template_directory() . '/acf-blocks/related-content-block/related-content.php',
+		'icon'				=> file_get_contents( get_template_directory() . '/acf-blocks/_block-assets/icon-related-content.svg' ),
+		'category' 			=> 'igb',
+		'example'  			=> array(
+			'attributes' 	=> array(
+				'mode' 		=> 'preview',
+				'data' 		=> array(
+					'is_preview'    => true
+				)
+			)
+		)
+	));
+
   }
 }
