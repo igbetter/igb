5abbe9f02baf14f81d31e8bf55f2b27b50135425
tracy apps <t@thisroad.org>
2023-11-29 20:36:58 -0600

    a11y slider, styles attempt

--- wp-content/themes/it-gets-better/_assets/js/src_scripts/a11y-slider-site.js
+++ wp-content/themes/it-gets-better/_assets/js/src_scripts/a11y-slider-site.js
@@ -2,7 +2,7 @@ const slider = new A11YSlider(document.querySelector('.horizontal_slider'), {
 	slidesToShow: 3,
 	arrows: true,
 	dots: false,
-	prevArrow: '<button class="left_arrow"><span class="a11y-slider-sr-only">Previous</span></button>',
-	nextArrow: '<button class="left_arrow"><span class="a11y-slider-sr-only">Next</span></button>',
+//	prevArrow: '<button class="left_arrow"><span class="a11y-slider-sr-only">Previous</span></button>',
+	//	nextArrow: '<button class="left_arrow"><span class="a11y-slider-sr-only">Next</span></button>',
 
 });
