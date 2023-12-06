b95c550b410f820d779a39d4a973bd077da364e2
tracy apps <t@thisroad.org>
2023-12-06 12:07:16 -0600

    more content block.. and some cleanup.

--- wp-content/themes/it-gets-better/_assets/js/src_scripts/splide.js
+++ wp-content/themes/it-gets-better/_assets/js/src_scripts/splide.js
@@ -1,35 +1,43 @@
- document.addEventListener( 'DOMContentLoaded', function() {
-	 var splide = new Splide('.splide', {
-		 perPage: 4,
-		 perMove: 1,
-		 gap: '2rem',
-		 height: 'auto',
-		 drag: 'free',
-		 noDrag: 'input, textarea, .no-drag',
-		 dragMinThreshold: {
-			mouse: 0,
-			touch: 10,
-		 },
-		 keyboard: true,
-		 wheel: true,
-		 releaseWheel: true,
-		 trimSpace: false,
-		 cover: true,
+document.addEventListener('DOMContentLoaded', function () {
 
-		 breakpoints: {
-			900: {
-				 perPage: 3,
-				 height: '150px',
-			 },
-			640: {
-				 perPage: 2,
-				 height: '125px',
+
+	var elms = document.getElementsByClassName('splide');
+	for (var i = 0; i < elms.length; i++) {
+		new Splide(elms[i] );
+		splide.mount();
+	}
+
+
+Splide.defaults = {
+		perPage: 4,
+			perMove: 1,
+			gap: '2rem',
+			height: 'auto',
+			drag: 'free',
+			noDrag: 'input, textarea, .no-drag',
+			dragMinThreshold: {
+				mouse: 0,
+				touch: 10,
 			},
-			480: {
-				perPage: 1,
-				height: '100px',
+			keyboard: true,
+			wheel: true,
+			releaseWheel: true,
+			trimSpace: false,
+			cover: true,
+
+			breakpoints: {
+				900: {
+					perPage: 3,
+					height: '150px',
+				},
+				640: {
+					perPage: 2,
+					height: '125px',
+				},
+				480: {
+					perPage: 1,
+					height: '100px',
+				},
 			},
-		},
-	});
-	 splide.mount();
-  } );
+	}
+});
--- wp-content/themes/it-gets-better/template-parts/landingPage/section-big-banner.php
+++
@@ -1,3 +0,0 @@
-<section class="big-banner px-default bg-igb-purple">
-	<p>Lorem ipsum dolor sit amet consectetur. Nunc consequat eu cursus tempus tristique. Risus turpis id tristique eget.</p>
-</section>
