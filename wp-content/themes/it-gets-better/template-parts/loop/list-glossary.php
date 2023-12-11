7bf0fe9e78704dd8a862e4fd22a4e42871b023aa
tracy apps <t@thisroad.org>
2023-12-11 04:19:17 -0600

    glossry page

--- wp-content/themes/it-gets-better/template-parts/loop/list-glossary.php
+++ wp-content/themes/it-gets-better/template-parts/loop/list-glossary.php
@@ -6,13 +6,41 @@
  $color = get_field( 'border_color' );
  $color_label = ( is_array( $color ) ? $color['label'] : $color );
 
- $glossary_term_category = get_field( 'concept_group' );
+$general_term_category = get_term_by( 'name', 'general', 'term-category');
+$general_term_category_ID = $general_term_category->term_id; // so we can exclude the "general" category in the arrays below
 
+$glossary_term_categories = get_the_terms( get_the_ID(), 'term-category');
+
+
+if( $glossary_term_categories ) :
+	// if this has term categories set, start an array to store the term_ids
+	$term_categories_ID_array = [];
+	foreach( $glossary_term_categories as $glossary_term_category ) :
+		$term_categories_ID_array[] = $glossary_term_category->term_id;
+	endforeach;
+
+	// now remove the 'general' term_id
+	$term_categories_ID_array = array_diff($term_categories_ID_array, array( $general_term_category_ID ) );
+endif;
 ?>
 
 <article id="glossary_term_ID-<?php echo esc_attr( get_the_ID() );?>" class="single_term color-<?php echo esc_attr( $color_label ); ?>">
 	<header class="glossary_term_header">
-		<h3 class="glossary_term"><?php the_title(); ?> <sub><?php esc_html_e( $glossary_term_category ); ?></sub></h3>
+		<h3 class="glossary_term"><?php the_title(); ?>
+			<?php
+				if ( $term_categories_ID_array ) :
+					echo '<sub class="term_category">';
+					foreach ( $term_categories_ID_array as $single_term_cat ) :
+						$acftermid = 'term-category_' . $single_term_cat;
+						$term_name = get_term( $single_term_cat, 'term-category' );
+						$term_name = $term_name->name;
+						echo '(<dfn class="term_category_def" title="' . esc_html( $term_name ) . '">' . get_field( 'sub_headline_abbreviation', $acftermid ) . '</dfn>)';
+					endforeach;
+					echo '</sub>';
+				endif;
+			?>
+		<sub><?php //esc_html_e( $glossary_term_category ); ?></sub>
+		</h3>
 	</header>
 	<main class="glossary_term_main">
 		<p><?php the_field( 'definition' ); ?></p>
