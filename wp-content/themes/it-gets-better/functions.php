4004209d2bc4ad0300260f063f8b9fe9707bbdaa
tracy apps <t@thisroad.org>
2024-05-28 21:28:59 -0500

    start of new new nav

--- wp-content/themes/it-gets-better/functions.php
+++ wp-content/themes/it-gets-better/functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'IT_GETS_BETTER_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'IT_GETS_BETTER_VERSION', '0.1.0' );
+	define( 'IT_GETS_BETTER_VERSION', '0.1.5' );
 }
 
 if ( ! function_exists( 'it_gets_better_setup' ) ) :
@@ -52,7 +52,10 @@ if ( ! function_exists( 'it_gets_better_setup' ) ) :
 		register_nav_menus(
 			array(
 				'utility-nav' => __( 'Utility Navigation', 'it-gets-better'),
-				'mega-nav-main' => __( 'Main Navigation', 'it-gets-better' ),
+				'nav-sections' => __( 'Navigation Section Toggle', 'it-gets-better' ),
+				'main-nav-find_support' => __( 'Main Nav: Find Support', 'it-gets-better' ),
+				'main-nav-get_involved' => __( 'Main Nav: Get Involved', 'it-gets-better' ),
+				'main-nav-about_us' => __( 'Main Nav: About Us', 'it-gets-better' ),
 				'footer-nav' => __( 'Footer Menu', 'it-gets-better' ),
 				'social-nav' => __( 'Social Links', 'it-gets-better' ),
 				'legal-nav' => __( 'Legal Nav (bottom)', 'it-gets-better')
--- wp-content/themes/it-gets-better/header.php
+++ wp-content/themes/it-gets-better/header.php
@@ -34,10 +34,44 @@
 
 <?php wp_body_open(); ?>
 
-<div id="page" class="sticky-container">
+<div id="page" class="sticky-container find_support">
 	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'it-gets-better' ); ?></a>
 
 	<header class="site_main_header">
+		<div id="site_utility_bar" class="flex-row">
+			<div class="utility_spacer"></div>
+			<nav id="section_select_menu" class="">
+				<ul>
+					<li>
+						<a href="#" id="select-find_support" class="find_support">Find Support</a>
+					</li>
+					<li>
+						<a href="#" id="select-get_involved" class="get_involved">Get Involved</a>
+					</li>
+					<li>
+						<a href="#" id="select-about_us" class="about_us">About Us</a>
+					</li>
+				</ul>
+			</nav>
+			<nav id="utility_navigation">
+				<?php
+					$utilityNavProps =  array(
+					'theme_location' => 'utility-nav',
+					'container'      => '',
+					'menu_class'     => 'utility_nav header',
+					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				);
+				wp_nav_menu( $utilityNavProps );
+				?>
+			</nav>
+			<label class="dark-toggle">
+					<span> Light </span>
+					<input type="checkbox">
+					<div class="dark-toggle__switch" tabindex="0"></div>
+					<span> Dark </span>
+				</label>
+
+		</div>
 		<div class="flex-row">
 			<div id="IGB_MAIN_LOGO">
 				<a href="<?php echo home_url(); ?>">
@@ -75,52 +109,47 @@
 				</svg>
 			</a>
 			</div>
-			<nav id="site_utility_nav" class="hidden">
-				<?php
-					$utilityNavProps =  array(
-					'theme_location' => 'utility-nav',
-					'container'      => '',
-					'menu_class'     => 'utility_nav header',
-					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				);
-				wp_nav_menu( $utilityNavProps );
-			?>
-			</nav>
+			<div class="header_center">
+				<nav id="subnav_for-find_support" class="section_main_nav">
+					<?php
+						$findSupportNavProps =  array(
+						'theme_location' => 'main-nav-find_support',
+						'container'      => '',
+						'menu_class'     => 'main-nav-find_support header',
+						'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+						'walker'		 => new Aria_Walker_Nav_Menu
+					);
+					wp_nav_menu( $findSupportNavProps );
+					?>
+				</nav>
+			</div>
 			<div class="header_right_side">
-				<label class="dark-toggle">
-					<span> Light </span>
-					<input type="checkbox">
-					<div class="dark-toggle__switch" tabindex="0"></div>
-					<span> Dark </span>
-				</label>
-				<div class="header_button_group">
+				<div id="site_search">
 					<?php get_template_part( 'template-parts/components/search-bar' ); ?>
-					<button id="menu_toggle_button" class="menu">
-						<svg
-							x="0" y="0" viewBox="0 0 40 40">
-							<path class="menu_line top_line"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m 15 10
-									l 20 0"/>
-							<path class="menu_line middle_line"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m 10 20
-									l 25 0"/>
-							<path class="menu_line bottom_line"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m 5 30
-									l 30 0"/>
-						</svg>
-					</button>
 				</div>
+				<button id="menu_toggle_button" class="menu">
+					<svg
+						x="0" y="0" viewBox="0 0 40 40">
+						<path class="menu_line top_line"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m 15 10
+								l 20 0"/>
+						<path class="menu_line middle_line"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m 10 20
+								l 25 0"/>
+						<path class="menu_line bottom_line"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m 5 30
+								l 30 0"/>
+					</svg>
+				</button>
 			</div>
 
 		</div>
-
-
 
 		<?php if( get_theme_mod ('safe_exit_url') !== "" && get_theme_mod('safe_exit_url_text') !== "" ): ?>
 			<div id="safeExit" class="safe-exit-button">
@@ -135,40 +164,7 @@
 		<?php endif; ?>
 
 	</header>
-	<div class="main-container">
-		<nav id="site_main_navigation">
-			<?php
-			$megaNavMainProps =  array(
-				'theme_location' => 'mega-nav-main',
-				'container'      => 'span',
-				'menu_class'     => 'IGB_site_main_navigation_menu header main-navigation',
-				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				'walker'		 => new Aria_Walker_Nav_Menu(),
-			);
 
-			wp_nav_menu( $megaNavMainProps );
-			?>
-		</nav>
-		<nav id="programs_nav">
-			<div class="program_box global">
-				<a href="/global/">
-					<object>
-						<h3>It Gets Better <span>Global</span></h3>
-						<p>It Gets Better is the world's largest storytelling effort to empower LGBTQ+ youth. Learn more about our global programs &amp; global affiliates here &raquo;</p>
-					</object>
-				</a>
-			</div>
-			<div class="program_box edu">
-				<a href="/education/">
-					<object>
-						<h3>It Gets Better <span>EDU</span></h3>
-						<p>It Gets Better EDU provides free educational resources for adults, educators, and student leaders. Explore our education resources &amp; programs here &raquo;</p>
-					</object>
-				</a>
-			</div>
-		</nav>
-
-	</div>
 	<?php if( !is_home() ) : ?>
 	<nav class="breadcrumb_navigation">
 		<?php // get_template_part( 'template-parts/components/page', 'breadcrumb'); ?>
