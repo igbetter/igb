9571be8fa4d9421861ccc588f44a4e98ead711fd
Christopher W. Jones <cjonessy03@gmail.com>
2024-06-08 13:37:09 -0400

    Edits for Modal - Glossary Download / EDUGuides
    Made a few CSS edits and theme file edits for the Modal on the glossary/ page and the eduguides/ page.

--- wp-content/themes/it-gets-better/template-parts/singular/eduguide.php
+++ wp-content/themes/it-gets-better/template-parts/singular/eduguide.php
@@ -52,7 +52,7 @@ endif;
 				$formidable_form_modal_anchor .= "<span class=\"file_size\">(&nbsp;";
 				$formidable_form_modal_anchor .= size_format( filesize( get_attached_file( $eduguide_download['ID'] ) ) );
 				$formidable_form_modal_anchor .= "&nbsp;)</span></a>";
-				echo do_shortcode( '[frmmodal-content size="large" button_html="<a data-js-action=\'formidable-form-popup\' class=\'primary_button filetype-' . esc_attr( $eduguide_download_file_type ) . '\' href=\'' . esc_url( $eduguide_download_url ) . '\'>Download the EduGuide<span class=\'file_size\'>(&nbsp;' . size_format( filesize( get_attached_file( $eduguide_download['ID'] ) ) ) . '&nbsp;)</span></a>"]' . '[formidable id=6]' . '[/frmmodal-content]' );
+				echo do_shortcode( '[frmmodal-content size="large" modal_title="Sign Up for Educational Resources" button_html="<a data-js-action=\'formidable-form-popup\' class=\'primary_button filetype-' . esc_attr( $eduguide_download_file_type ) . '\' href=\'' . esc_url( $eduguide_download_url ) . '\'>Download the EduGuide<span class=\'file_size\'>(&nbsp;' . size_format( filesize( get_attached_file( $eduguide_download['ID'] ) ) ) . '&nbsp;)</span></a>"]' . '[formidable id=6]' . '[/frmmodal-content]' );
 			?>
 			<script type="text/javascript">
 			  document.addEventListener('DOMContentLoaded', function () {
