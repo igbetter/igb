e16f60712d94bc83afb3389a90fdb68aafc99e32
tracy apps <t@thisroad.org>
2024-07-31 22:13:21 -0500

    cleanup unused menu toggle JS

--- wp-content/themes/it-gets-better/_assets/js/src_scripts/nav-toggle.js
+++ wp-content/themes/it-gets-better/_assets/js/src_scripts/nav-toggle.js
@@ -1,41 +1,6 @@
 (function($) {
 	"use strict";
 
-	// menu changer
-	$(document).ready(function () {
-		// Retrieve the stored class name from localStorage
-		var storedClass = localStorage.getItem('selectedMenu');
-		if (storedClass) {
-			// Apply the stored class to the page and menu
-			$('#page').addClass('sticky-container ' + storedClass);
-			$('#section_select_menu').addClass('selected_' + storedClass);
-		} else {
-			$('#page').addClass('sticky-container find_support');
-			$('#section_select_menu').addClass('selected_find_support');
-		}
-
-		$('#section_select_menu a').click(function (e) {
-			e.preventDefault();
-			var id = $(this).attr('id'); // Get the ID of the clicked anchor link
-			var className = id.split('-')[1]; // Extract the class name from the ID
-			$('#page').removeClass().addClass('sticky-container ' + className); // Remove existing classes and add the new class
-			$('#section_select_menu').removeClass().addClass('selected_' + className);
-
-			// Store the selected class name in localStorage
-			localStorage.setItem('selectedMenu', className);
-		});
-
-		$('.section_main_nav h3').click(function (e) {
-			e.preventDefault();
-			var className = this.className // Get the class name
-			$('#page').removeClass().addClass('sticky-container ' + className); // Remove existing classes and add the new class
-			$('#section_select_menu').removeClass().addClass('selected_' + className);
-
-			// Store the selected class name in localStorage
-			localStorage.setItem('selectedMenu', className);
-		});
-	});
-
 	//dropdown toggle
 	$(document).ready(function() {
 		$('.dropdown_toggle').on('click', function(e) {
