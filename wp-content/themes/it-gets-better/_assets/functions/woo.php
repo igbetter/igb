4216c1fd01a9ac7a6eed6704710f87ec938e6387
TommusRhodus <tom.rhodes@automattic.com>
2025-04-09 15:08:54 +0100

    Update woo.php

--- wp-content/themes/it-gets-better/_assets/functions/woo.php
+++ wp-content/themes/it-gets-better/_assets/functions/woo.php
@@ -5,8 +5,25 @@
  *
  */
 
- add_filter( 'woocommerce_single_product_image_thumbnail_html', 'igb_remove_product_link' );
+add_filter( 'woocommerce_single_product_image_thumbnail_html', 'igb_remove_product_link' );
 
- function igb_remove_product_link( $html ) {
-   return strip_tags( $html, '<div><img>' );
- }
+function igb_remove_product_link( $html ) {
+	return strip_tags( $html, '<div><img>' );
+}
+
+/**
+ * Change the "Add to Cart" button text for out of stock products.
+ *
+ * @param string $text Original text.
+ * @param WC_Product $product Product object.
+ *
+ * @return string
+ */
+function igb_out_of_stock_button_text( $text, $product ) {
+	if ( ! $product->is_in_stock() ) {
+		return esc_html__( 'Sold Out', 'it-gets-better' );
+	}
+
+	return $text;
+}
+add_filter( 'woocommerce_product_add_to_cart_text', 'igb_out_of_stock_button_text', 10, 2 );
